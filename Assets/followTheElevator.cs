07f2c0e161e677c71bf32351756ed75c6c4101a4
Mathijs Jansen <mathijs@code.rehab>
2019-03-23 17:10:35 +0100

    elevator01 scene added

---
+++ Assets/followTheElevator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followTheElevator : MonoBehaviour
+{
+    public Vector3 myPos;
+    public Vector3 additionalHeight;
+    public Transform myPlay;
+    
+    public void Update()
+    {
+        transform.position = myPlay.position + (myPos + additionalHeight);
+    }
+
+}
