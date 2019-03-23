648dface1b6349d997abea2f2e7c24001a438cbe
Mathijs Jansen <mathijs@code.rehab>
2019-03-23 21:30:59 +0100

    freefall

--- Assets/moveElevator.cs
+++ Assets/moveElevator.cs
@@ -8,6 +8,8 @@ public class moveElevator : MonoBehaviour
 
     public GameObject elevator;
     public float elavatorY = 0;
+    public float levelHeight = 0;
+    public int level = 0;
 
     void Start()
     {
@@ -17,8 +19,30 @@ public class moveElevator : MonoBehaviour
 
     void NextFloor() {
         Debug.Log("we are moving to the next floor");
-        elavatorY = elavatorY + 6;
+        level++;
+        
+
+        if (level == 1) levelHeight = 12;
+        if (level == 2) levelHeight = 6;
+        if (level == 3) levelHeight = 20;
+        Debug.Log(level);
+        Debug.Log(levelHeight);
+
+   
+        elavatorY = elavatorY + levelHeight;
+
+        if (level == 3) {
+            TweenY.Add(elevator, 4f, elavatorY).EaseInOutSine().Delay(5f).Then(FreeFall);
+            return;
+        }
         TweenY.Add(elevator, 4f, elavatorY).EaseInOutSine().Delay(5f).Then(NextFloor);
+
+    }
+
+    void FreeFall() {
+
+        Debug.Log("i am freefallin;");
+        TweenY.Add(elevator, 2f, 18).EaseInSine().Delay(5f);
     }
 
 }
