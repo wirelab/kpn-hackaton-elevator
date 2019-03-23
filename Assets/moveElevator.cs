e5cad29c4dffbf6ad707f8bc4423d5deff9b696c
Mathijs Jansen <mathijs@code.rehab>
2019-03-23 20:18:22 +0100

    uWeen animation library added

--- Assets/moveElevator.cs
+++ Assets/moveElevator.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Uween;
 
 public class moveElevator : MonoBehaviour
 {
+
+    public GameObject elevator;
     void Update()
     {
-        transform.Translate(Vector3.forward * (Time.deltaTime / 3));
+        // transform.Translate(Vector3.forward * (Time.deltaTime / 3));
+
+        // log timeframe        
+        TweenY.Add(elevator, 5f, 10f);
+        // Debug.Log(Time.deltaTime);
     }
 }
