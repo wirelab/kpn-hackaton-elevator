5ca3f7f0480f3926f4d67d5b9329cc536fb5f8f7
harmvdwerf@gmail.com <harmvdwerf@gmail.com>
2019-03-24 13:11:29 +0100

    Added drone and altered platform blades script

--- Assets/Scripts/rotatePlatformBlades.cs
+++ Assets/Scripts/rotatePlatformBlades.cs
@@ -21,10 +21,21 @@ public class rotatePlatformBlades : MonoBehaviour
     void rotateBlades()
     {
         // Debug.Log("Rotate the blades now");
-        TweenRY.Add(engine1, 40f, 9200f).EaseInOutSine();
-        TweenRY.Add(engine2, 40f, 9200f).EaseInOutSine();
-        TweenRY.Add(engine3, 40f, 9200f).EaseInOutSine();
-        TweenRY.Add(engine4, 40f, 9200f).EaseInOutSine();
+        // TweenRY.Add(engine1, 40f, 9200f);
+        // TweenRY.Add(engine2, 40f, 9200f);
+        // TweenRY.Add(engine3, 40f, 9200f);
+        // TweenRY.Add(engine4, 40f, 9200f);
+
+        startTween(engine1);
+        startTween(engine2);
+        startTween(engine3);
+        startTween(engine4);
+    }
+
+    void startTween(GameObject engine) {
+        TweenRY.Add(engine, 1f, 360f).Then(delegate() {
+            startTween(engine);
+        });
     }
 
 }
