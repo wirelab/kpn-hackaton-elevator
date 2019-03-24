be287f3af43185319f538033c413ae776d911a93
harmvdwerf@gmail.com <harmvdwerf@gmail.com>
2019-03-24 01:51:10 +0100

    Added shaky cam

--- Assets/moveElevator.cs
+++ Assets/moveElevator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Uween;
+using EZCameraShake;
 
 public class moveElevator : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class moveElevator : MonoBehaviour
     public float watchTime;
     public bool lockCamera = false;
 
+    float freeFallTime = 2f;
+
     void Start()
     {
         goToNextFloor();
@@ -23,36 +26,37 @@ public class moveElevator : MonoBehaviour
 
         if (level == 1) {
             watchTime = 5f;
-          
-            nextLevelHeight = 12;            
+
+            nextLevelHeight = 12;
         }
 
         if (level == 2) {
             watchTime = 5f;
             rotatePlatform();
-            nextLevelHeight = 6;            
+            nextLevelHeight = 6;
         }
         if (level == 3) {
-            watchTime = 5f;
-            nextLevelHeight = 20;            
+            watchTime = 0.5f;
+            nextLevelHeight = 20;
         }
 
         elavatorY = elavatorY + nextLevelHeight;
 
         // the end
         if (level == 3) {
-            TweenY.Add(elevator, 4f, elavatorY).EaseInOutSine().Delay(watchTime).Then(GoFreeFall);
+            TweenY.Add(elevator, 0.1f, elavatorY).EaseInOutSine().Delay(watchTime).Then(GoFreeFall);
             return;
         }
 
-        TweenY.Add(elevator, 4f, elavatorY).EaseInOutSine().Delay(watchTime).Then(goToNextFloor);
+        TweenY.Add(elevator, 0.1f, elavatorY).EaseInOutSine().Delay(watchTime).Then(goToNextFloor);
 
     }
 
     void GoFreeFall() {
-
         // Debug.Log("i am freefallin;");
-        TweenY.Add(elevator, 2f, 18).EaseInCubic().Delay(5f);
+        float delay = 1f;
+        TweenY.Add(elevator, freeFallTime, 18).EaseInCubic().Delay(delay).Then(ShakeFloor);
+        TweenNull.Add(elevator, delay + 0.5f).Then(ShakeFreeFall);
     }
 
     void rotatePlatform()
@@ -66,7 +70,13 @@ public class moveElevator : MonoBehaviour
 
     void Update()
     {
-    
+
+    }
+    void ShakeFreeFall() {
+        CameraShaker.Instance.ShakeOnce(2f, 7f, freeFallTime, 0.01f);
+    }
+    void ShakeFloor() {
+        CameraShaker.Instance.ShakeOnce(12, 2.5f, 0f, 2f);
     }
 
 }
