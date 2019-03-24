92f8199148e1c3e0979d286d50583236a399bd09
Mathijs Jansen <mathijs@code.rehab>
2019-03-24 13:37:28 +0100

    added sound manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        foreach (Sound s in sounds) {
+           AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+           s.source = audioSource;
+           s.source.clip = s.clip;
+           s.source.pitch = s.pitch;
+           s.source.volume = s.volume; 
+        }
+        
+    }
+
+    private void Start()
+    {
+        Play("ambient");
+    }
+
+    public void Play(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+
+    }
+}
