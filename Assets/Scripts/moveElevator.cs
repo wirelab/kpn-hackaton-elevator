7c0fb91631c5965078300ccd813254d0e3f14fa3
Mathijs Jansen <mathijs@code.rehab>
2019-03-24 16:55:25 +0100

    show hide

--- Assets/Scripts/moveElevator.cs
+++ Assets/Scripts/moveElevator.cs
@@ -20,7 +20,7 @@ public class moveElevator : MonoBehaviour
 
     void Start()
     {
-        
+       //  FindObjectOfType<ComponentSwitcher>().Hide("Kabel en Tekst V2");
     }
 
     void addHeight(float height) {
@@ -91,7 +91,7 @@ public class moveElevator : MonoBehaviour
         float freeFallTime = 2f;
         gotoHeight(18);
 
-        FindObjectOfType<ComponentSwitcher>().Show("Line018");
+        // FindObjectOfType<ComponentSwitcher>().Show("Kabel en Tekst V2");
 
         FindObjectOfType<AudioManager>().Stop("narration-city");
         FindObjectOfType<AudioManager>().Play("free-fall");
